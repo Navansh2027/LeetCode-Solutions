3d72369e6c8436dd6c661da5a0a486c5d2e3b6ae
Navansh_kumawat <168294264+Navansh2027@users.noreply.github.com>
2026-08-18 20:56:56 +0530

    Time: 0 ms (100.00%) | Memory: 29.1 MB (78.95%) - LeetSync

---
+++ 3705-find-the-largest-almost-missing-integer/find-the-largest-almost-missing-integer.cpp
@@ -0,0 +1,63 @@
+class Solution {
+public:
+    int largestInteger(vector<int>& nums, int k) {
+        int n = nums.size();
+
+        // Case 1: Every element is a subarray of size 1
+        if (k == 1) {
+            unordered_map<int, int> freq;
+
+            for (int x : nums) {
+                freq[x]++;
+            }
+
+            int ans = -1;
+
+            for (auto& [x, count] : freq) {
+                if (count == 1) {
+                    ans = max(ans, x);
+                }
+            }
+
+            return ans;
+        }
+
+        // Case 2: Only one subarray exists
+        if (k == n) {
+            return *max_element(nums.begin(), nums.end());
+        }
+
+        // Case 3: 1 < k < n
+        int ans = -1;
+
+        // Check first element
+        bool firstUnique = true;
+
+        for (int i = 1; i < n; i++) {
+            if (nums[i] == nums[0]) {
+                firstUnique = false;
+                break;
+            }
+        }
+
+        if (firstUnique) {
+            ans = max(ans, nums[0]);
+        }
+
+        // Check last element
+        bool lastUnique = true;
+
+        for (int i = 0; i < n - 1; i++) {
+            if (nums[i] == nums[n - 1]) {
+                lastUnique = false;
+                break;
+            }
+        }
+
+        if (lastUnique) {
+            ans = max(ans, nums[n - 1]);
+        }
+
+        return ans;
+    }
+};
