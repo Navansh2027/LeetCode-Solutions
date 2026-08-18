8822c6abd65c6df7e29518bb8feba3ad9de0a724
Navansh_kumawat <168294264+Navansh2027@users.noreply.github.com>
2026-08-18 21:03:07 +0530

    Time: 0 ms (100.00%) | Memory: 28.9 MB (84.21%) - LeetSync

--- 3705-find-the-largest-almost-missing-integer/find-the-largest-almost-missing-integer.cpp
+++ 3705-find-the-largest-almost-missing-integer/find-the-largest-almost-missing-integer.cpp
@@ -3,18 +3,21 @@ public:
     int largestInteger(vector<int>& nums, int k) {
         int n = nums.size();
 
-        // Case 1: Every element is a subarray of size 1
-        if (k == 1) {
+        if (k == 1) 
+        {
             unordered_map<int, int> freq;
 
-            for (int x : nums) {
+            for (int x : nums) 
+            {
                 freq[x]++;
             }
 
             int ans = -1;
 
-            for (auto& [x, count] : freq) {
-                if (count == 1) {
+            for (auto& [x, count] : freq) 
+            {
+                if (count == 1) 
+                {
                     ans = max(ans, x);
                 }
             }
@@ -22,33 +25,35 @@ public:
             return ans;
         }
 
-        // Case 2: Only one subarray exists
-        if (k == n) {
+        if (k == n) 
+        {
             return *max_element(nums.begin(), nums.end());
         }
 
-        // Case 3: 1 < k < n
         int ans = -1;
 
-        // Check first element
         bool firstUnique = true;
 
-        for (int i = 1; i < n; i++) {
-            if (nums[i] == nums[0]) {
+        for (int i = 1; i < n; i++) 
+        {
+            if (nums[i] == nums[0]) 
+            {
                 firstUnique = false;
                 break;
             }
         }
 
-        if (firstUnique) {
+        if (firstUnique) 
+        {
             ans = max(ans, nums[0]);
         }
 
-        // Check last element
         bool lastUnique = true;
 
-        for (int i = 0; i < n - 1; i++) {
-            if (nums[i] == nums[n - 1]) {
+        for (int i = 0; i < n - 1; i++) 
+        {
+            if (nums[i] == nums[n - 1]) 
+            {
                 lastUnique = false;
                 break;
             }
